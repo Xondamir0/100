08a2e833d2d57530f3b7502a84ee8f52f48cced1
Xondamir0 <alimjanovxondamir01@gmail.com>
2025-01-21 23:51:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+document.body.style.backgroundColor = "blue";
+
+const userName = prompt("Ism kiriting");
+
+const newTag = document.createElement("h1");
+newTag.textContent = userName;
+
+newTag.style.textAlign = "center";
+newTag.style.color = "yellow";
+
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+document.body.style.alignItems = "center";
+document.body.style.height = "100vh";
+
+document.body.append(newTag);
+
+
+
+// newTag.style.backgroundColor = "red";
+// const userName = prompt("Rang kiriting")
+
+// const newTa = document.createElement("div")
+
+
+
+
+
+
+
+// const width = prompt("Width kiriting")
+// const height = prompt("Height kiriting")
+// const bg = prompt("Background color kiriting")
+
+// const newDiv = document.createElement("div")
+
+// newDiv.style.width = width + "px";
+
+// newDiv.style.height = height + "px";
+
+// newDiv.style.backgroundColor = bg;
+
+// document.body.append(newDiv)
+
+
+
+
+
+
+
+
+
+
+
+
